19db4fa25e335111658ce847515e56bfe751d740
sebas <sebaslpez0519@gmail.com>
2021-11-16 18:18:39 -0500

    Funcionalidad de rebote de pelota con las barras

--- main.js
+++ main.js
@@ -32,17 +32,46 @@
         this.board.ball=this;
         this.kind="circle"
         this.direccion=1;
+        this.speed=5;
+        this.bounce_angle=0;
+        this.max_bounce_angle=Math.PI/12;
     }
     self.Ball.prototype={
         move: function(){
             this.x+=(this.speed_x * this.direccion );
             this.y+=(this.speed_y);
         },
+        collision: function (bar) {
+            var relative_intersect_y= (bar.y +(bar.height/2))-this.y;
+            console.log(bar.y);
+            console.log(bar.height);
+            console.log(this.y);
+            console.log(relative_intersect_y);
+            var normalized_intersect_y=relative_intersect_y / (bar.height/2);
+            console.log(normalized_intersect_y);
+            this.bounce_angle=normalized_intersect_y*this.max_bounce_angle;
+            console.log(this.max_bounce_angle);
+            console.log( this.bounce_angle);
+            this.speed_y=this.speed *-Math.sin(this.bounce_angle);
+            this.speed_x=this.speed * Math.cos(this.bounce_angle);
+
+            if(this.x>(this.board.width/2)) this.direccion=-1;
+            else this.direccion=1;
+        },
+        collisionBorder: function () {
+        
+            this.bounce_angle=2;
+            this.speed_x=this.speed *-Math.sin(this.bounce_angle);
+            this.speed_y=this.speed * Math.cos(this.bounce_angle);
+        },
+        get width(){
+            return this.radius*2;
+        },
+        get height(){
+            return this.radius*2;
+        }
     }
 
-    /*self.Ball.prototype{
-
-    }*/
 })();
 
 
@@ -95,10 +124,55 @@
         if(this.board.playing){
         this.clean();
         this.draw();
+        this.check_collisions();
         this.board.ball.move();
         }
+    },
+    check_collisions: function () {
+        for (var i = this.board.bars.length - 1; i >= 0; i--) {
+            var bar = this.board.bars[i];
+            if(hit(bar,this.board.ball)){
+                this.board.ball.collision(bar);
+            }
+            if(hitBorder(this.board.ball)){
+                this.board.ball.collisionBorder();
+            }
+            
+          }
     }
   };
+
+  function hitBorder(ball) {
+    var hit = false;
+    if(ball.y+ball.height<=50){
+        hit=true;
+    }
+    if(ball.y+ball.height>=300){
+        hit=true;
+    }
+    return hit;
+  }
+
+  function hit(a,b) {
+      var hit = false;
+
+      if(b.x+b.width >=a.x && b.x<a.x+a.width){
+          if(b.y + b.height>=a.y && b.y <a.y+a.height ){
+              hit=true;
+          }
+      }
+      if(b.x <= a.x && b.x +b.width>=a.x +a.width){
+          if(b.y<=a.y && b.y+b.height>=a.y+a.height){
+                hit=true;
+          }
+      }
+      if(a.x<=b.x && a.x +a.width>=b.x +b.width){
+          if(a.y<=b.y && a.y + a.height >= b.y +b.height){
+              hit=true;
+          }
+      }
+      return hit;
+  }
 
   function draw(ctx, element) {
     //hasOwnProperty dice si tiene una propiedad kind
